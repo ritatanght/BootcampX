05eefee713773a6a5bcd69e17c0ed351ac572a19
Rita Tang <doggymint@gmail.com>
2023-09-18 13:15:59 -0400

    Add students without github query for 0_selects folder

---
+++ 0_selects/1_students_without_github.sql
@@ -0,0 +1,8 @@
+-- Get all students without a Github username.
+-- Select their id, name, email, and cohort_id.
+-- Order them by cohort_id.
+
+SELECT id, name, email, cohort_id
+FROM students
+  WHERE github is NULL
+  ORDER BY cohort_id;
