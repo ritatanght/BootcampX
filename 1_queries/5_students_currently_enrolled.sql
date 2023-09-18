80a8e963996c311e36b3c4a3426a121c4c18ad53
Rita Tang <doggymint@gmail.com>
2023-09-18 14:17:49 -0400

    Add queries for 1_queries/

---
+++ 1_queries/5_students_currently_enrolled.sql
@@ -0,0 +1,8 @@
+-- Get all of the students currently enrolled.
+-- Get their name, id, and cohort_id.
+-- Order them by cohort_id.
+
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
---
+++ 1_queries/6_students_without_linked_github.sql
@@ -0,0 +1,7 @@
+-- Get all graduates without a linked Github account.
+-- Get their name, email, and phone.
+
+SELECT name, email, phone
+FROM students
+WHERE end_date IS NOT NULL
+AND github IS NULL;
